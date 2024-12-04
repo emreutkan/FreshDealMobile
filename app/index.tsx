ccb1713c8a34533544c43241a45835b41c4d46af
emreutkan <127414322+emreutkan@users.noreply.github.com>
2024-12-05 00:41:32 +0300

    registerPage

--- app/index.tsx
+++ app/index.tsx
@@ -16,7 +16,7 @@ const Index = () => {
         // are completed before the navigation logic is triggered.
         const timer = setTimeout(() => {
             // Navigate to the LoginPage after the slight asynchronous break
-            router.push('../screens/loginRegister/loginPage');
+            router.push('/screens/loginPage');
         }, 0); // 0ms delay ensures minimal delay while still deferring execution
 
         // Cleanup function to clear the timer when the component unmounts
