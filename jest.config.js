809782d94eb58fc3353c6f982cfd6ea925ec3918
Irfan Emre Utkan <127414322+emreutkan@users.noreply.github.com>
2025-05-25 22:05:01 +0300

    feat: update Jest configuration to use react-native preset and jsdom environment

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 /** @type {import('jest').Config} */
 const config = {
-    preset: 'jest-expo',
-    setupFiles: ['<rootDir>/__tests__/setup.ts'],
+    // Replace jest-expo preset with a custom preset configuration
+    preset: 'react-native',
+    setupFiles: [
+        '<rootDir>/__tests__/setup.ts',
+        // Add react-native mocks that would normally be included in jest-expo
+        './node_modules/react-native-gesture-handler/jestSetup.js'
+    ],
     transformIgnorePatterns: [
         'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
     ],
@@ -12,12 +17,22 @@ const config = {
         '**/__tests__/**/*.test.[jt]s?(x)'
     ],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    testEnvironment: 'node',
+    // Use jsdom instead of node environment for React components
+    testEnvironment: 'jsdom',
     transform: {
         '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {
             presets: ['babel-preset-expo']
         }]
     },
+    // Setting rootDir explicitly
+    rootDir: '.',
+    // Add required configurations that jest-expo would normally provide
+    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+        '!**/node_modules/**',
+        '!**/coverage/**'
+    ],
     globals: {
         'ts-jest': {
             tsconfig: {
@@ -28,3 +43,4 @@ const config = {
 };
 
 module.exports = config;
+
